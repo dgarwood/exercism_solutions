4603cf3273207e77ac49174a52d5edf9628341eb
Dewey Garwood <dewey.garwood@gmail.com>
2017-12-01 18:43:01 -0700

    remove redundant steps sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -5,15 +5,21 @@ end
 class Sieve
   def initialize(number)
     @num = number
-    @primes = []
-    @values = (2..@num).to_a
   end
 
   def primes
-    until @values.empty?
+    @primes = []
+    @values = (3..@num).step(2).to_a
+
+    # only sieve if we have @values
+    # and stop when first_term^2 > last_term (nothing left to remove)
+    # ie if @num = 100, stop at 11 since 11^2 = 121
+    until @values.empty? || @values[0]**2 > @values[-1]
       @values = @values.reject{ |n| n % @values[0] == 0 && n / @values[0] > 1 }
       @primes << @values.shift
     end
-    @primes
+
+    @primes += @values # all remaining @values are @primes
+    @num < 2 ? @primes : @primes.unshift(2) # don't forget 2
   end
 end

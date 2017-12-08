b352439d4fb821cb0575cb31fd6c2de512fb3761
Dewey Garwood <dewey.garwood@gmail.com>
2017-12-08 15:32:10 -0700

    syntax cleanup

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,7 @@
 module Hamming
   def self.compute(source, diff)
     raise ArgumentError, "strands not same length" unless source.length == diff.length
-    (0..(source.length - 1)).count do |i|
-      source[i] != diff[i]
-    end
+    (0...source.length).count{ |i| source[i] != diff[i] }
   end
 end
 

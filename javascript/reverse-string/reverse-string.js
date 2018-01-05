6b22b9666eb9987cebde07a0422c465266735e4d
Dewey Garwood <dewey.garwood@gmail.com>
2018-01-04 20:58:21 -0700

    remove unneeded conditional

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,13 +1,7 @@
 'use strict'
 
 var ReverseString = function (string) {
-  if (string.length === 0) {
-    return string
-  }
-
-  string = string.split('')
-  string = string.reverse()
-  return string.join('')
+  return string.split('').reverse().join('')
 }
 
 module.exports = ReverseString
